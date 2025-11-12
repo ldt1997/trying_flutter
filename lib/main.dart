dc4b8714bdef4711d8f6bbb5ec2ac1ab96a69988
ldt1997 <474376875@qq.com>
2025-11-12 21:47:18 +0100

    feat: add navigation

--- lib/main.dart
+++ lib/main.dart
@@ -48,45 +48,80 @@ class MyAppState extends ChangeNotifier {
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [
+          SafeArea(
+            child: NavigationRail(
+              extended: false,
+              destinations: [
+                NavigationRailDestination(
+                  icon: Icon(Icons.home),
+                  label: Text('Home'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.favorite),
+                  label: Text('Favorites'),
+                ),
+              ],
+              selectedIndex: 0,
+              onDestinationSelected: (value) {
+                print('selected: $value');
+              },
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              child: GeneratorPage(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
 
     IconData icon;
-    if(appState.favorites.contains(pair)) {
-      icon =  Icons.favorite;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
     } else {
-      icon =  Icons.favorite_border;
+      icon = Icons.favorite_border;
     }
 
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            BigCard(pair: pair),
-            SizedBox(height: 24),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ElevatedButton.icon(
-                  onPressed: () {
-                    appState.toggleFavorite();
-                  },
-                  label: Text('Like'),
-                  icon: Icon(icon),
-                ),
-                SizedBox(width: 12),
-                ElevatedButton.icon(
-                  onPressed: () {
-                    appState.getNext();
-                  },
-                  label: Text('Next'),
-                  icon: Icon(Icons.arrow_right),
-                ),
-              ],
-            ),
-          ],
-        ),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(pair: pair),
+          SizedBox(height: 24),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavorite();
+                },
+                icon: Icon(icon),
+                label: Text('Like'),
+              ),
+              SizedBox(width: 12),
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.getNext();
+                },
+                label: Text('Next'),
+                icon: Icon(Icons.arrow_right),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
